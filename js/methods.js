8db598f895236dd38ae417941803cab2ff2eae68
byhag <byhag4007@gmail.com>
2016-12-28 06:00:30 -0600

    Add search and sidebar

--- js/methods.js
+++ js/methods.js
@@ -1,9 +1,23 @@
-function DoThing() {
-    console.log("Doing a thing");
-}
-
-var windowWidth = window.innerWidth;
-
 $(document).ready( function() {
-    
+    $('.submenu').hide();
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(".submenu-toggle").click(function(e) {
+        console.log(screen.width);
+        e.preventDefault();
+        $(this).next('.submenu').slideToggle();
+        console.log(screen.width);
+        if (screen.width <= 768) {
+            console.log('toggle');
+            $('#page-content-wrapper').toggle(function(){
+                this.css('overflow-y','hidden');
+            }, function () {
+                this.css('overflow-y','auto');
+            });
+        }
+    });
 });
