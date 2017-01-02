af521e5669e12bf2f882bf9607a03fa7eb66b696
byhag <byhag4007@gmail.com>
2017-01-02 01:51:11 -0600

    Add truthtree; add leaf icon

--- js/methods.js
+++ js/methods.js
@@ -1,6 +1,9 @@
 $(document).ready( function() {
+    var duration = 300;
+
     $('.submenu').hide();
     $('.sharing').hide();
+    $('.select-menu').hide();
 
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
@@ -9,11 +12,28 @@ $(document).ready( function() {
 
     $(".submenu-toggle").click(function(e) {
         e.preventDefault();
-        $(this).next('.submenu').slideToggle();
+        $(this).next('.submenu').slideToggle(duration);
     });
 
     $(".sharing-toggle").click(function(e) {
         e.preventDefault();
-        $(this).next('.sharing').slideToggle();
+        $(this).next('.sharing').slideToggle(duration);
+    });
+
+    $(".select-toggle").click(function(e) {
+        e.preventDefault();
+        if (!$(this).next('.select-menu').is(":visible")) {
+            $('.select-menu').slideUp(duration);
+            $('.select-toggle').css('border-radius','5px');
+            $(this).css('border-bottom-left-radius','0px');
+            $(this).css('border-bottom-right-radius','0px');
+        }
+        var toggle = $(this);
+        toggle.next('.select-menu').slideToggle(duration, function () {
+           if (!toggle.next('.select-menu').is(":visible")) {
+                toggle.css('border-bottom-left-radius','5px');
+                toggle.css('border-bottom-right-radius','5px');
+            }
+        });
     });
 });
